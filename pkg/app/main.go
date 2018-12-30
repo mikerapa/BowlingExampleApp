dd81f12564aebf4307419b982ea0526fe23a6243
Mike Rapa <Mike@mikerapa.com>
2018-12-30 16:10:26 -0500

    Initial commit

---
+++ pkg/app/main.go
@@ -0,0 +1,12 @@
+package app
+
+import "fmt"
+
+// import bowling "github.com/mikerapa/bowlingscores/src/bowlingscore"
+
+// StartMessage : Should be the first function called
+func StartMessage() {
+	fmt.Println("Application running")
+	// rolls := []string{"X", "2", "/", "8", "/", "7", "1", "X", "8", "/", "9", "/", "8", "-", "X", "8", "/", "5"}
+	// bowling.bowlingscores.GetGameStatsFromRolls(rolls)
+}
